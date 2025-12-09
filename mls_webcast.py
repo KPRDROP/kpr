d326efee5077b4939ff3dd698619fff3073e94c1
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2025-12-09 11:21:50 -0400

    Update mls_webcast.py

--- mls_webcast.py
+++ mls_webcast.py
@@ -12,8 +12,10 @@ from playwright.async_api import async_playwright, TimeoutError as PlaywrightTim
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
 
-referer = "https://mlswebcast.com/",
-origin ="https://mlswebcast.com"
+referer = 
+    "https://mlswebcast.com/",
+origin = 
+    "https://mlswebcast.com"
 
 (
     
