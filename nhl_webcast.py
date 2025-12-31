3639a31b473fb97dd8c3cb069da849eae3dd9319
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2025-12-30 21:58:38 -0400

    Update nhl_webcast.py

--- nhl_webcast.py
+++ nhl_webcast.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
 
 USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0"
 )
 
 BASE = "https://slapstreams.com/"
