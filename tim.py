7b19b28425d4ade6ffd70853373411324c631347
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-03-10 09:36:58 -0400

    Update tim.py

--- tim.py
+++ tim.py
@@ -6,7 +6,7 @@ from urllib.parse import urljoin, quote
 from playwright.async_api import Browser, Page, Response
 from selectolax.parser import HTMLParser
 
-from .utils import Cache, Time, get_logger, leagues, network
+from utils import Cache, Time, get_logger, leagues, network
 
 log = get_logger(__name__)
 
