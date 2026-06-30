c5dde38cfd54b90441b2d9ec5685753930c4ffcb
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-06-30 13:42:56 -0400

    Update centerstrm.py

--- centerstrm.py
+++ centerstrm.py
@@ -23,8 +23,10 @@ EMBED_BASE = "https://streams.center/"
 
 CATEGORIES = {
     #4: "Basketball",
-    9: "Football",
-    13: "Baseball",
+    #9: "Football",
+    9: "FIFA World Cup",,
+    #13: "Baseball",
+    13: "MLB",
     #14: "American Football",
     15: "Motor Sport",
     #16: "Hockey",
