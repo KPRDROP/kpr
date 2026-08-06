fb51fbe88391e1cce36bb2bc152275a2e2d048b1
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-08-06 17:50:56 -0400

    Replace HTMLParser with LexborHTMLParser

--- webtvmlb.py
+++ webtvmlb.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from urllib.parse import urljoin, quote_plus
 
 from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
-from selectolax.parser import HTMLParser
+from selectolax.lexbor import LexborHTMLParser as HTMLParser
 
 # ================= CONFIG =================
 
